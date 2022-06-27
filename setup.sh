882bcb29d6a41514dfc3e40682365f48a3605bdd
Huyen Nguyen <48179958+huyenngn@users.noreply.github.com>
2022-06-27 14:16:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-downloads=/home/sofia/Downloads/
+downloads=/home/sofia/Downloads
 
 
 sudo apt-get install -y --no-install-recommends build-essential ninja-build wget file python3 pkg-config libgmp-dev libmpfr-dev libmpc-dev libexpat-dev libfdt-dev libglib2.0-dev libpixman-1-dev
@@ -19,8 +19,10 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt install --reinstall nvidia-driver-470 
 sudo prime-select nvidia
 
+sudo snap remove --purge firefox
+
 sudo apt-get install git autokey-gtk
-rm -rf $downloads/gnome-macorm -rf $downloads/gnome-macos-remap
+rm -rf $downloads"/gnome-macorm" -rf $downloads/gnome-macos-remap
 git clone https://github.com/petrstepanov/gnome-macos-remap $downloads
 chmod +x $downloads"/gnome-macos-remap/install.sh" $downloads"/gnome-macos-remap/uninstall.sh"
 sh $downloads"/gnome-macos-remap/install.sh"
